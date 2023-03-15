7bcc78e606812c56cf0374f7890b52e0ce95041a
manvithshetty507 <manvithshetty18@gmail.com>
2023-03-15 09:27:23 +0530

    sixth

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,29 +1,17 @@
 package com.driver;
 
 public class Email {
-
     private String emailId;
     private String password;
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
     }
 
-    public String getEmailId() {
-        return emailId;
+    public String getEmailId() { return emailId;
     }
-
-    public String getPassword() {
-        return password;
+    public String getPassword() { return password;
     }
 
     public void changePassword(String oldPassword, String newPassword){
@@ -33,27 +21,30 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(this.password.equals(oldPassword)){
-            boolean length=false,uc=false,lc=false,dig=false,sp=false;
-            if(newPassword.length() >= 8) length = true;
-            for(int i=0;i<newPassword.length();i++){
-                char c = newPassword.charAt(i);
-                if(c > 97 && c < 124){
-                    lc = true;
-                }
-                else if(c > 64 && c <91){
-                    uc = true;
-                }
-                else if(c > 47 && c < 58){
-                    dig = true;
-                }
-                else{
-                    sp = true;
-                }
+        boolean containsUpperCase=false;
+        boolean containsLowerCase=false;
+        boolean containsADigit=false;
+        boolean containsSpecialChar=false;
+        if(oldPassword.equals(this.password)){
+
+            if(newPassword.length()<8) return; //checks if it contains atleat 8 characters
+            for(char c:newPassword.toCharArray()){
+                if(c>='A' && c<='Z') containsUpperCase=true;
+
+                if(c>='a' && c<='z') containsLowerCase=true;
+
+                if(c>='0' && c<='9') containsADigit=true;
+
+                if(!(c>='0' && c<='9') && !(c>='a' && c<='z') && !(c>='A' && c<='Z') ) containsSpecialChar=true;
             }
-            if(length && uc && lc && sp && dig)
-                this.password = newPassword;
-            return;
+
+            //all criterials satisfied then change password
+            if(containsUpperCase && containsLowerCase && containsADigit && containsSpecialChar){
+
+                this.password=newPassword;
+            }
+            else return;
+
         }
     }
 }
